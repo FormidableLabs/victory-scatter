f323dc4cad77029ac8e1dccfd9b1f624d977e6b8
Angela Nicholas <angelaknicholas@gmail.com>
2015-08-05 17:31:09 -0700

    basic scatter

--- demo/app.jsx
+++ demo/app.jsx
@@ -1,22 +1,71 @@
 /*global document:false*/
 import React from "react";
+import Radium from "radium";
+import _ from "lodash";
 import {VictoryScatter} from "../src/index";
 
+const getData = function () {
+  let seriesNumber = "Three";
+
+  return _.map(_.range(100), () => {
+    switch (seriesNumber) {
+      case "One":
+        seriesNumber = "Two";
+        break;
+      case "Two":
+        seriesNumber = "Three";
+        break;
+      case "Three":
+        seriesNumber = "One";
+        break;
+      // no default
+    }
+
+    return { x: _.random(1200), y: _.random(600), z: "series" + seriesNumber };
+  });
+};
+
+@Radium
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: this.props.data,
+      height: 600,
+      dotColors: ["pink", "lightblue", "gold"],
+      width: 1200
+    };
+  }
+
+  getStyles() {
+    return {
+      border: "1px solid #ccc",
+      height: this.state.height,
+      margin: "0 auto",
+      width: this.state.width
+    };
+  }
+
   render() {
     return (
-      <div className="demo">
-        <p>
-          < VictoryScatter/>
-        </p>
-        <p>
-          < VictoryScatter color={"red"}/>
-        </p>
+      <div style={this.getStyles()}>
+        <VictoryScatter
+          data={this.state.data}
+          dotColors={this.state.dotColors}
+          height={this.state.height}
+          width={this.state.width}/>
       </div>
     );
   }
 }
 
-const content = document.getElementById("content");
+App.propTypes = {
+  data: React.PropTypes.arrayOf(React.PropTypes.shape({
+    x: React.PropTypes.number,
+    y: React.PropTypes.number,
+    z: React.PropTypes.string
+  }))
+};
 
-React.render(<App/>, content);
+const content = document.getElementById("content");
+React.render(<App data={getData()}/>, content);
--- src/components/victory-scatter.jsx
+++ src/components/victory-scatter.jsx
@@ -1,35 +1,67 @@
 import React from "react";
 import Radium from "radium";
-
+import d3 from "d3";
+import _ from "lodash";
 
 @Radium
 class VictoryScatter extends React.Component {
-  getStyles() {
-    return {
-      base: {
-        color: "#000",
-        fontSize: 12,
-        textDecoration: "underline"
-      },
-      red: {
-        color: "#d71920",
-        fontSize: 30
-      }
-    };
+  constructor(props) {
+    super(props);
+    this.colors = d3.scale.ordinal().range(this.props.dotColors);
+  }
+
+  drawDots() {
+    const dotComponents = _.map(this.props.data, (dot, index) => {
+      return (
+        <circle
+          cx={dot.x}
+          cy={this.props.height - dot.y}
+          fill={this.colors(dot.z)}
+          key={index}
+          r={3.5}
+          stroke={this.props.borderColor}/>
+      );
+    });
+
+    return (<g>{dotComponents}</g>);
   }
 
   render() {
-    const styles = this.getStyles();
     return (
-      <div style={[styles.base, this.props.color === "red" && styles.red]}>
-        Edit me!
-      </div>
+      <svg
+        height={this.props.height}
+        width={this.props.width}>
+        {this.drawDots()}
+      </svg>
     );
   }
 }
 
 VictoryScatter.propTypes = {
-  color: React.PropTypes.string
+  borderColor: React.PropTypes.string,
+  data: React.PropTypes.arrayOf(React.PropTypes.shape({
+    x: React.PropTypes.number,
+    y: React.PropTypes.number,
+    z: React.PropTypes.string
+  })),
+  dotColors: React.PropTypes.arrayOf(React.PropTypes.string),
+  height: React.PropTypes.number,
+  width: React.PropTypes.number
+};
+
+VictoryScatter.defaultProps = {
+  borderColor: "black",
+  data: [
+    { x: 20, y: 20, z: "sampleSeriesOne" },
+    { x: 30, y: 30, z: "sampleSeriesOne" },
+    { x: 100, y: 100, z: "sampleSeriesTwo" },
+    { x: 110, y: 110, z: "sampleSeriesTwo" },
+    { x: 200, y: 200, z: "sampleSeriesThree" },
+    { x: 210, y: 210, z: "sampleSeriesThree" }
+  ],
+  dotColors: ["pink", "lightblue", "gold"],
+  height: 800,
+  width: 1200
 };
 
 export default VictoryScatter;
