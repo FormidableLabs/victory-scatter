47367245f8dfeb45d59a49c7c0b70fa5b1b8f021
Lauren <lauren.eastridge@formidablelabs.com>
2015-12-31 16:06:37 -0800

    use methods from util

--- src/components/victory-scatter.jsx
+++ src/components/victory-scatter.jsx
@@ -1,4 +1,4 @@
-import React, { PropTypes } from "react";
+import React from "react";
 import Radium from "radium";
 import take from "lodash/array/take";
 import union from "lodash/array/union";
@@ -16,7 +16,7 @@ import min from "lodash/math/min";
 import range from "lodash/utility/range";
 import d3Scale from "d3-scale";
 import Point from "./point";
-import Util from "victory-util";
+import {PropTypes, Chart, Data, Scale} from "victory-util";
 import {VictoryAnimation} from "victory-animation";
 
 const defaultStyles = {
@@ -46,12 +46,12 @@ export default class VictoryScatter extends React.Component {
      * Large datasets might animate slowly due to the inherent limits of svg rendering.
      * @examples {delay: 5, velocity: 0.02, onEnd: () => alert("woo!")}
      */
-    animate: PropTypes.object,
+    animate: React.PropTypes.object,
     /**
      * The bubbleProperty prop indicates which property of the data object should be used
      * to scale data points in a bubble chart
      */
-    bubbleProperty: PropTypes.string,
+    bubbleProperty: React.PropTypes.string,
     /**
      * The data prop specifies the data to be plotted. Data should be in the form of an array
      * of data points where each data point should be an object with x and y properties.
@@ -59,10 +59,10 @@ export default class VictoryScatter extends React.Component {
      * These properties will be interpreted and applied to the individual lines
      * @examples [{x: 1, y: 2, fill: "red"}, {x: 2, y: 3, label: "foo"}]
      */
-    data: PropTypes.arrayOf(
-      PropTypes.shape({
-        x: PropTypes.any,
-        y: PropTypes.any
+    data: React.PropTypes.arrayOf(
+      React.PropTypes.shape({
+        x: React.PropTypes.any,
+        y: React.PropTypes.any
       })
     ),
     /**
@@ -73,56 +73,56 @@ export default class VictoryScatter extends React.Component {
      * available information.
      * @examples [-1, 1], {x: [0, 100], y: [0, 1]}
      */
-    domain: PropTypes.oneOfType([
-      Util.PropTypes.domain,
-      PropTypes.shape({
-        x: Util.PropTypes.domain,
-        y: Util.PropTypes.domain
+    domain: React.PropTypes.oneOfType([
+      PropTypes.domain,
+      React.PropTypes.shape({
+        x: PropTypes.domain,
+        y: PropTypes.domain
       })
     ]),
     /**
      * The height props specifies the height of the chart container element in pixels
      */
-    height: Util.PropTypes.nonNegative,
+    height: PropTypes.nonNegative,
     /**
      * The labelComponent prop takes in an entire, HTML-complete label component
      * which will be used to create labels for scatter to use
      */
-    labelComponent: PropTypes.element,
+    labelComponent: React.PropTypes.element,
     /**
      * The maxBubbleSize prop sets an upper limit for scaling data points in a bubble chart
      */
-    maxBubbleSize: Util.PropTypes.nonNegative,
+    maxBubbleSize: PropTypes.nonNegative,
     /**
      * The padding props specifies the amount of padding in number of pixels between
      * the edge of the chart and any rendered child components. This prop can be given
      * as a number or as an object with padding specified for top, bottom, left
      * and right.
      */
-    padding: PropTypes.oneOfType([
-      PropTypes.number,
-      PropTypes.shape({
-        top: PropTypes.number,
-        bottom: PropTypes.number,
-        left: PropTypes.number,
-        right: PropTypes.number
+    padding: React.PropTypes.oneOfType([
+      React.PropTypes.number,
+      React.PropTypes.shape({
+        top: React.PropTypes.number,
+        bottom: React.PropTypes.number,
+        left: React.PropTypes.number,
+        right: React.PropTypes.number
       })
     ]),
     /**
      * The samples prop specifies how many individual points to plot when plotting
      * y as a function of x. Samples is ignored if x props are provided instead.
      */
-    samples: Util.PropTypes.nonNegative,
+    samples: PropTypes.nonNegative,
     /**
      * The scale prop determines which scales your chart should use. This prop can be
      * given as a function, or as an object that specifies separate functions for x and y.
      * @exampes d3Scale.time(), {x: d3Scale.linear(), y:tick d3Scale.log()}
      */
-    scale: PropTypes.oneOfType([
-      Util.PropTypes.scale,
-      PropTypes.shape({
-        x: Util.PropTypes.scale,
-        y: Util.PropTypes.scale
+    scale: React.PropTypes.oneOfType([
+      PropTypes.scale,
+      React.PropTypes.shape({
+        x: PropTypes.scale,
+        y: PropTypes.scale
       })
     ]),
     /**
@@ -131,20 +131,20 @@ export default class VictoryScatter extends React.Component {
      * If animations are running slowly, try setting this prop to false to cut down on
      * the number of svg nodes
      */
-    showLabels: PropTypes.bool,
+    showLabels: React.PropTypes.bool,
     /**
      * The size prop determines how to scale each data point
      */
-    size: PropTypes.oneOfType([
-      Util.PropTypes.nonNegative,
-      PropTypes.func
+    size: React.PropTypes.oneOfType([
+      PropTypes.nonNegative,
+      React.PropTypes.func
     ]),
     /**
      * The standalone prop determines whether the component will render a standalone svg
      * or a <g> tag that will be included in an external svg. Set standalone to false to
      * compose VictoryScatter with other components within an enclosing <svg> tag.
      */
-    standalone: PropTypes.bool,
+    standalone: React.PropTypes.bool,
     /**
      * The style prop specifies styles for your scatter plot. VictoryScatter relies on Radium,
      * so valid Radium style objects should work for this prop. Height, width, and
@@ -152,31 +152,31 @@ export default class VictoryScatter extends React.Component {
      * are used to calculate the alignment of components within chart.
      * @examples {parent: {margin: 50}, data: {fill: "red"}, labels: {padding: 20}}
      */
-    style: PropTypes.shape({
-      parent: PropTypes.object,
-      data: PropTypes.object,
-      labels: PropTypes.object
+    style: React.PropTypes.shape({
+      parent: React.PropTypes.object,
+      data: React.PropTypes.object,
+      labels: React.PropTypes.object
     }),
     /**
      * The symbol prop determines which symbol should be drawn to represent data points.
      */
-    symbol: PropTypes.oneOfType([
-      PropTypes.oneOf([
+    symbol: React.PropTypes.oneOfType([
+      React.PropTypes.oneOf([
         "circle", "diamond", "plus", "square", "star", "triangleDown", "triangleUp"
       ]),
-      PropTypes.func
+      React.PropTypes.func
     ]),
     /**
      * The width props specifies the width of the chart container element in pixels
      */
-    width: Util.PropTypes.nonNegative,
+    width: PropTypes.nonNegative,
     /**
      * The x prop provides another way to supply data for scatter to plot. This prop can be given
      * as an array of values, and it will be plotted against whatever y prop is provided. If no
      * props are provided for y, the values in x will be plotted as the identity function.
      * @examples [1, 2, 3]
      */
-    x: Util.PropTypes.homogeneousArray,
+    x: PropTypes.homogeneousArray,
     /**
      * The y prop provides another way to supply data for scatter to plot. This prop can be given
      * as a function of x, or an array of values. If x props are given, they will be used
@@ -184,9 +184,9 @@ export default class VictoryScatter extends React.Component {
      * evenly spaced across the x domain will be calculated, and used for plotting data points.
      * @examples (x) => Math.sin(x), [1, 2, 3]
      */
-    y: PropTypes.oneOfType([
-      PropTypes.array,
-      PropTypes.func
+    y: React.PropTypes.oneOfType([
+      React.PropTypes.array,
+      React.PropTypes.func
     ])
   };
 
@@ -205,12 +205,12 @@ export default class VictoryScatter extends React.Component {
 
   getCalculatedValues(props) {
     this.style = this.getStyles(props);
-    this.padding = this.getPadding(props);
+    this.padding = Chart.getPadding(props);
     this.range = {
-      x: this.getRange(props, "x"),
-      y: this.getRange(props, "y")
+      x: Chart.getRange(props, "x"),
+      y: Chart.getRange(props, "y")
     };
-    this.data = this.getData(props);
+    this.data = Data.getData(props);
     this.domain = {
       x: this.getDomain(props, "x"),
       y: this.getDomain(props, "y")
@@ -231,36 +231,11 @@ export default class VictoryScatter extends React.Component {
     };
   }
 
-  getPadding(props) {
-    const padding = isNumber(props.padding) ? props.padding : 0;
-    const paddingObj = isObject(props.padding) ? props.padding : {};
-    return {
-      top: paddingObj.top || padding,
-      bottom: paddingObj.bottom || padding,
-      left: paddingObj.left || padding,
-      right: paddingObj.right || padding
-    };
-  }
-
   getScale(props, axis) {
-    let scale;
-    if (props.scale && props.scale[axis]) {
-      scale = props.scale[axis].copy();
-    } else if (props.scale && !isObject(props.scale)) {
-      scale = props.scale.copy();
-    } else {
-      scale = d3Scale.linear().copy();
-    }
+    const scale = Scale.getBaseScale(props, axis);
     scale.range(this.range[axis]);
     scale.domain(this.domain[axis]);
     return scale;
-  }
-
-  getRange(props, axis) {
-    // if the range is not given in props, calculate it from width, height and margin
-    return axis === "x" ?
-      [this.padding.left, props.width - this.padding.right] :
-      [props.height - this.padding.bottom, this.padding.top];
   }
 
   getDomain(props, axis) {
@@ -274,50 +249,6 @@ export default class VictoryScatter extends React.Component {
         max(pluck(this.data, axis))
       ];
     }
-  }
-
-  getData(props) {
-    if (props.data) {
-      return props.data;
-    }
-    const x = this.returnOrGenerateX(props);
-    const y = this.returnOrGenerateY(props, x);
-    const n = min([x.length, y.length]);
-    // create a dataset from x and y with n points
-    const dataset = zip(take(x, n), take(y, n));
-    // return data as an array of objects
-    return dataset.map((point) => {
-      return {x: point[0], y: point[1]};
-    });
-  }
-
-  returnOrGenerateX(props) {
-    if (props.x) {
-      return props.x;
-    }
-    // if x is not given in props, create an array of values evenly
-    // spaced across the x domain
-    const domainFromProps = props.domain && props.domain.x || props.domain;
-    const domainFromScale = props.scale && props.scale.x ?
-      props.scale.x.domain() : props.scale.domain();
-    const domain = domainFromProps || domainFromScale;
-
-    const samples = isArray(props.y) ? props.y.length : props.samples;
-    const step = max(domain) / samples;
-    // return an array of x values spaced across the domain,
-    // include the maximum of the domain
-    return union(range(min(domain), max(domain), step), [max(domain)]);
-  }
-
-  returnOrGenerateY(props, x) {
-    if (isFunction(props.y)) {
-      // if y is a function, apply the function y to to each value of the array x,
-      // and return the results as an array
-      return x.map((datum) => props.y(datum));
-    }
-    // y is either a function or an array, and is never undefined
-    // if it isn't a function, just return it.
-    return props.y;
   }
 
   getSymbol(data) {
